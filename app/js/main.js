75ab0cdc71c7075b7dff08057f4c6d30004e1c23
Artem Dolbiev <dolbievartem@gmail.com>
2020-12-03 02:10:52 +0200

    Верстка секции header

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,16 @@ $(function(){
 
     $('.header__slider').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
     });
 
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
 
 });
